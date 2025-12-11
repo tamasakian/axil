05ebc28bafee84ee330f991068ece068a4ceb6b0
Masaki Takagawa <takagawa.masaki.q2@dc.tohoku.ac.jp>
2025-12-11 11:32:53 +0900

    feat: add run_blastp function

--- libs/tools/diamond.zsh
+++ libs/tools/diamond.zsh
@@ -74,3 +74,63 @@ function blastp_for_mcscanx() {
     log_info "all ${total_times} blast runs completed successfully"
     return 0
 }
+
+function run_blastp() {
+    local input_taskname="$1"
+    local input_taskdir="${TASKS}/${input_taskname}"
+    if [ ! -d "$input_taskdir" ]; then
+        log_error "input task directory not found: ${input_taskdir}"
+        return 1
+    fi
+
+    local config_file="${input_taskdir}/config.zsh"
+    if ! parse_config "$config_file"; then
+        log_error "configuration failed"
+        return 1
+    fi
+
+    if [ -z "$REF_FASTA" ] || [ -z "$QRY_FASTA" ]; then
+        log_error "REF_FASTA or QRY_FASTA are missing"
+        return 1
+    fi
+
+    : ${DIAMOND_THREADS:=1}
+    log_info "use DIAMOND_THREADS=${DIAMOND_THREADS}"
+
+    : ${BLAST_EVALUE:="1e-5"}
+    log_info "Us3 BLAST_EVALUE=${BLAST_EVALUE}"
+
+    local ref_fasta="${input_taskdir}/${REF_FASTA}"
+    local qry_fasta="${input_taskdir}/${QRY_FASTA}"
+    if [ ! -f "$ref_fasta" ] || [ ! -f "$qry_fasta" ]; then
+        log_error "one or both input fasta files not found"
+        return 1
+    fi
+
+    local timestamp=$(date +"%Y%m%d%H%M%S")
+    local output_taskname="P1-run_blastp-${timestamp}"
+    local output_taskdir="${TASKS}/${output_taskname}"
+    mkdir -p "$output_taskdir"
+
+    local db_file="${output_taskdir}/ref.dmnd"
+    if ! diamond makedb --in "$ref_fasta" -d "$db_file"; then
+        log_error "$context" "Diamond DB creation failed."
+        return 1
+    fi
+
+    local output_file="${output_taskdir}/blastp.results"
+    if ! diamond blastp \
+        -q "$qry_fasta" \
+        -d "$db_file" \
+        -o "$output_file" \
+        --evalue "$BLAST_EVALUE" \
+        --threads "$DIAMOND_THREADS" \
+        --outfmt 6; then
+
+        log_error "diamond blastp execution failed"
+        return 1
+    fi
+
+    log_info "blastp completed successfully"
+    return 0
+}
